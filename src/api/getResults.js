c9e42481766a599dad483bbcbc7446e59027eb19
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-12-03 13:49:14 +0000

    Changes naming from str to term

--- src/api/getResults.js
+++ src/api/getResults.js
@@ -1,16 +1,16 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
-export async function getResults(str) {
-  if (!str) {
+export async function getResults(term) {
+  if (!term) {
     return [];
   }
-  let getApiResults = await getResultsFromApi(str);
+  let getApiResults = await getResultsFromApi(term);
   return getApiResults;
 }
 
-export async function getResultsFromApi(str) {
+export async function getResultsFromApi(term) {
   try {
-    const data = await fetch(`${apiUrl}/contacts?term=${str}`);
+    const data = await fetch(`${apiUrl}/contacts?term=${term}`);
     const contacts = await data.json();
     return contacts;
   } catch (error) {
